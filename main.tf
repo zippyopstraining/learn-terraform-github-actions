dd86b4ac0c63f80b07570be584ef1123a668ee35
root <root@demo.zippyops.com>
2021-02-25 00:35:25 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "zippyops"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Zippyops_demo_gitaction"
     }
   }
 }
